4646fef5184b7025026393641e67e13a02606d7c
Shaurya9935 <prakhargupta9935@gmail.com>
2026-07-12 21:37:20 +0530

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,5 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+  import { StyleSheet, Text, View } from 'react-native';
+
+
+function onBoardingPage() {
+
+}
 
 export default function App() {
   return (
---
+++ src/screens/on-boarding-page.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+export function onBoardingPage() {
+    return (
+    <View>
+         <Text>On boarding page</Text>
+    </View>
+ )
+}
